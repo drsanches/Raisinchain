9ce125d5da97d6121bcc76c4b3d30be16db60bf8
extralucile <marina.a.krylova@gmail.com>
2018-03-17 17:32:13 +0300

    Added exception handling

--- server/src/main/java/main/AddingTransactionController.java
+++ server/src/main/java/main/AddingTransactionController.java
@@ -1,10 +1,10 @@
 package main;
 
+import containers.TransactionsList;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import containers.TransactionsList;
 import containers.Transaction;
 
 
@@ -18,17 +18,22 @@ public class AddingTransactionController {
 
 
         try {
+            
             //get a list of transactions, add a new one and save them all to json file
             TransactionsList list = Application.transactionsList;
             list.addTransaction(new Transaction(transaction));
             list.saveToJsonFile(Application.TRANSACTIONS_FILENAME);
 
-            return new ResponseEntity<String>(transaction, responseHeaders, HttpStatus.CREATED);
+            return new ResponseEntity<String>(HttpStatus.OK);
         }
-        catch(Exception e) {
+        catch(org.springframework.http.converter.HttpMessageNotReadableException e) {
             return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+        }
+        catch(java.io.IOException ex){
+            return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
 
 }
+
