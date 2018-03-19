1f1b1d3532a12a300e25e62a4af1dbb60291c253
ilushakr <kreshkov1002@gmail.com>
2018-03-19 17:08:50 +0300

    BlockChainTest

--- server/src/test/groovy/containers/BlockChainTest.groovy
+++ server/src/test/groovy/containers/BlockChainTest.groovy
@@ -3,13 +3,43 @@ package containers
 import spock.lang.Specification
 
 class BlockChainTest extends Specification {
+    Random rnd = new Random()
+
+
     def "Ensure that method getChain returns field ArrayList<Block>"() {
         given:"List of blocks"
-        ArrayList<Block> Array_List =Mock()
+//        List<Block> Array_List = [block()]
+        List<Block> list = new ArrayList<Block>()
+
         when:"put values to new chain"
-        BlockChain Block_Chain= new BlockChain(Array_List)
+        BlockChain Block_Chain= new BlockChain(list)
+
         then: "Method getChain returns value of field ArrayList<Block>"
-        Array_List.equals(Block_Chain.getChain())
+        list.equals(Block_Chain.getChain())
+    }
+
+
+    def "Ensure that method getPartChain returns field ArrayList<Block>"() {
+        given:"List of blocks"
+//        List<Block> Array_List = [block()]
+        List<Block> list1 = [block()]
+        List<Block> list3 = [block()]
+        List<Block> list4 = [*list3, block()]
+        List<Block> list2 = [*list1, *list4,]
+
+
+        when:"put values to new chain"
+        BlockChain Block_Chain= new BlockChain(list2)
+        String h = list1.get(0).hashCode
+        List<Block> y=Block_Chain.getPartOfChain(h)
+
+        then: "Method getPartChain returns value of field ArrayList<Block>"
+        list4.equals(y)
+    }
+
+    Block block() {
+        String hash = "${rnd.nextInt()}"
+        new Block(null, hash)
     }
 
     def "Ensure that method add new block to chain"() {
