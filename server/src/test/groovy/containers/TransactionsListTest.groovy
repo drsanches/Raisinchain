047d2025cad39be524459aa3af1af54a8199412b
anastasiia <nshal3879@gmail.com>
2018-03-19 15:31:22 +0300

    Test for removeTransaction method added

--- server/src/test/groovy/containers/TransactionsListTest.groovy
+++ server/src/test/groovy/containers/TransactionsListTest.groovy
@@ -16,8 +16,38 @@ class TransactionsListTest extends Specification {
 
     }
 
-    def "RemoveTransaction"() {
+    /*  def "Test for AddTransaction method"() {
+           given: "list of transactions"
+           TransactionsList tr_list = Mock()
+           //int size = tr_list.sizeOfList()
+   
+           when: "we add new transaction to list of transactions"
+           Transaction new_tr = new Transaction("new_transaction")
+           1*tr_list.addTransaction() >> 'new_tr'
+   
+           then: "transaction added to the list"
+           tr_list.addTransaction() == 'new_tr'
+   
+   
+       }*/
+
+    def "Test for removeTransaction method"() {
+        given: "non-empty list of transactions"
+        TransactionsList tr_list = new TransactionsList()
+        Transaction new_tr = new Transaction("new_transaction")
+        Transaction new_tr2 = new Transaction("second_transaction")
+        tr_list.addTransaction(new_tr)
+        tr_list.addTransaction(new_tr2)
+
+        when: "we remove a transaction"
+        tr_list.removeTransaction(new_tr)
+
+        then: "the list became shorter for one transaction"
+        tr_list.sizeOfList() == 1
+
     }
+
+ 
 
     def "CreateFirstTransactionsList"() {
     }
