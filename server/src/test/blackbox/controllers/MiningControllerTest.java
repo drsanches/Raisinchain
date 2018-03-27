5936de57d1485c700676356e5d296be05cf8efaf
anastasiia <nshal3879@gmail.com>
2018-03-27 17:13:42 +0300

    Request in MiningController changed

--- server/src/test/blackbox/controllers/MiningControllerTest.java
+++ server/src/test/blackbox/controllers/MiningControllerTest.java
@@ -31,7 +31,7 @@ public class MiningControllerTest extends BaseTest {
         HashMap<String, String> query1 = new HashMap<>();
         query1.put("WrongParameter", "Value");
 
-        Response response = sendPost("/gethash",query1);
+        Response response = sendPost("/calculatehash",query1);
 
         int responseStatus = response.getStatusCode();
         String responseHeader = response.getHeader("Access-Control-Allow-Origin");
@@ -47,7 +47,7 @@ public class MiningControllerTest extends BaseTest {
         query2.put("Block", "Value1");
         query2.put("Block2","Value2");
 
-        Response response = sendPost("/gethash",query2);
+        Response response = sendPost("/calculatehash",query2);
 
         int responseStatus = response.getStatusCode();
         String responseHeader = response.getHeader("Access-Control-Allow-Origin");
@@ -61,7 +61,7 @@ public class MiningControllerTest extends BaseTest {
         Block block = RandomContainerCreator.createBlockWithRandomHashCode();
         HashMap<String, String> query = new HashMap<>();
         query.put("Block", block.getJsonObject().toString());
-        Response response = sendPost("/gethash", query);
+        Response response = sendPost("/calculatehash", query);
 
         int responseStatus = response.getStatusCode();
         String responseHeader = response.getHeader("Access-Control-Allow-Origin");
